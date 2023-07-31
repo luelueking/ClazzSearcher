152022c0e995e8dfa58adc4d31993fd3ba4a4ccd
陈再 <2336485988@qq.com>
2023-07-31 15:31:59 +0800

    添加部分模板

--- src/test/java/org/vidar/test/TestYaml.java
+++ src/test/java/org/vidar/test/TestYaml.java
@@ -10,8 +10,8 @@ public class TestYaml {
     public static void main(String[] args) throws FileNotFoundException {
         // 读取 YAML 文件
 //        FileInputStream input = new FileInputStream("/Users/zhchen/Downloads/github-workspace/ClazzSearcher/src/main/resources/template.yml");
-//        FileInputStream input = new FileInputStream("/Users/zhchen/Downloads/github-workspace/ClazzSearcher/src/main/resources/BeanMetadataAttribute.yml");
-        FileInputStream input = new FileInputStream("/Users/zhchen/Downloads/github-workspace/ClazzSearcher/src/main/resources/JndiDataSourceAutoConfiguration.yml");
+//        FileInputStream input = new FileInputStream("/Users/zhchen/Downloads/github-workspace/ClazzSearcher/src/main/resources/parent.yml");
+        FileInputStream input = new FileInputStream("/Users/zhchen/Downloads/github-workspace/ClazzSearcher/src/main/resources/method.yml");
         // 创建 SnakeYAML 解析器
         Yaml yaml = new Yaml();
         // 将 YAML 转换为 Java 对象
